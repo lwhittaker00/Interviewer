9385a541e768f1da820fea01ea5999a96802c2f6
lwhittaker00 <latejah.whittaker.938@my.csun.edu>
2017-07-10 16:48:50 -0700

    Initial Commit
    Currently has login screen design, and files are organized

---
+++ Interviewer/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  Login.swift
+//  Interviewer
+//
+//  Created by Macbook on 7/10/17.
+//  Copyright © 2017 LC. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+}
+
